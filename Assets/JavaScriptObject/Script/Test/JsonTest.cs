ef7e02ff7f899bf84b5859c05181bb5dc8b7d572
zhangzhanyi <zhangzhanyi@i-focusing.com>
2019-05-22 13:04:17 +0800

    整体测试代码

--- Assets/JavaScriptObject/Script/Test/JsonTest.cs
+++ Assets/JavaScriptObject/Script/Test/JsonTest.cs
@@ -2,43 +2,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class JsonTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        JavaScriptObjectTest();
+    }
+
+    private void JavaScriptObjectTest()
+    {
+        //创建一个JavaScriptObject对象
         JavaScriptObject jsonObj = JavaScriptObjectFactory.CreateJavaScriptObject(Resources.Load<TextAsset>("test").text);
-        Debug.Log("转换为json:"+jsonObj.ToString());
-        Debug.Log("判断code字段是否存在:"+jsonObj.ContainsKey("code"));
-        Debug.Log("判断test字段是否存在:"+jsonObj.ContainsKey("test"));
-        Debug.Log("获取子对象data:"+jsonObj["data"]);
-        Debug.Log("获取子对象data的name字段:" + jsonObj["data"]["name"]);
-        Debug.Log("修改data的值");
+        Debug.Log("----------------转换为JSON---------------");
+        Debug.Log(jsonObj.ToJson());
+        Debug.Log("----------------判断code属性是否存在---------------");
+        Debug.Log(jsonObj.ContainsKey("code"));
+        Debug.Log("----------------获取子对象data---------------");
+        Debug.Log("获取子对象data:" + jsonObj["data"].ToJson());
+        Debug.Log("----------------获取子对象data的name属性---------------");
+        Debug.Log("获取子对象data的name字段:" + jsonObj["data"]["name"].GetString());
+        Debug.Log("----------------修改data的值---------------");
         jsonObj["data"] = new JavaScriptObject(1);
-        Debug.Log("获取子对象data:" + jsonObj["data"]);
-        //添加一个新的字段
+        Debug.Log("----------------修改后data的值---------------");
+        Debug.Log(jsonObj["data"].GetNumber());
+        Debug.Log("----------------添加一个新的Number属性test add num---------------");
         jsonObj.Add("test add num", 1);
+        Debug.Log(jsonObj["test add num"].GetNumber());
+        Debug.Log("----------------添加一个新的boolean属性test add boolean---------------");
         jsonObj.Add("test add boolean", false);
+        Debug.Log(jsonObj["test add boolean"].GetBoolean());
+        Debug.Log("----------------添加一个新的string属性---------------");
         jsonObj.Add("test add string", "new string");
+        Debug.Log(jsonObj["test add string"].GetString());
+        Debug.Log("----------------添加一个数组属性test add array---------------");
         JavaScriptObject array = new JavaScriptObject();
         array.Add(1);
         array.Add(2);
         array.Add(3);
         jsonObj.Add("test add array", array);
+        Debug.Log(jsonObj["test add array"].ToJson());
+        Debug.Log("----------------添加一个object属性test add subObject---------------");
         JavaScriptObject subObj = new JavaScriptObject();
         subObj.Add("sub obj num", 1);
         subObj.Add("sub obj boolean", false);
         jsonObj.Add("test add subObject", subObj);
-        Debug.Log("转换为json:" + jsonObj.ToJson());
-        Debug.Log("-----------遍历----------");
+        Debug.Log(jsonObj["test add subObject"].ToJson());
+        Debug.Log("----------------修改后的json---------------");
+        Debug.Log(jsonObj.ToJson());
+        Debug.Log("----------------遍历所有根属性---------------");
         foreach (var item in jsonObj)
         {
             Debug.Log(item.Key + ":" + item.Value);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+
+}
+
+class HelloWorld
+{
+    public string message;
+    public int id;
+    public bool isSuccess;
+
+    public HelloWorld()
+    {
+    }
+
+    public HelloWorld(string message, int id, bool isSuccess)
+    {
+        this.message = message;
+        this.id = id;
+        this.isSuccess = isSuccess;
+    }
+
+    public override string ToString()
+    {
+        return "message:"+message+";id:"+id+";isSuccess:"+isSuccess;
+    }
 }
