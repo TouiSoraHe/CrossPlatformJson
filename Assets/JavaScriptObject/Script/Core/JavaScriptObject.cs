6726eff32a2788d389824defac4ce69a2094255a
zhangzhanyi <zhangzhanyi@i-focusing.com>
2019-07-15 10:11:52 +0800

    *#;修复json序列化时的一个空指针异常

--- Assets/JavaScriptObject/Script/Core/JavaScriptObject.cs
+++ Assets/JavaScriptObject/Script/Core/JavaScriptObject.cs
@@ -367,6 +367,7 @@
         private static readonly char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         private static string StringifyString(string value)
         {
+            if (value == null) return "null";
             StringBuilder str = new StringBuilder();
             str.Append("\"");
             for (int i = 0; i < value.Length; i++)
